4b4e71cc6236d974a43a89f802a4dc79d5207555
Tristan Shah <123.tristan.shah@gmail.com>
2021-11-02 12:44:10 -0400

    made prototype graph embedding

--- main.jl
+++ main.jl
@@ -1,8 +1,48 @@
 using DesignEnvironments
 using ReinforcementLearning
-ENV["GKSwstype"] = "nul"
+# ENV["GKSwstype"] = "nul"
+using Flux
 
-env = CylinderEnv(M=10, continuous=true, grid_size=10.0)
-policy = RandomPolicy(action_space(env))
+struct GraphEmb
+    emb::Matrix
+end
 
-render(env, policy, path="thing.mp4")
+function GraphEmb(emb_dim::Int, graph_dim::Int)
+    emb = randn(graph_dim, emb_dim)
+    return GraphEmb(emb)
+end
+
+Flux.@functor GraphEmb
+
+function (m::GraphEmb)(x)
+    emb = m.emb
+
+    x = batched_mul(x, emb)
+    x = sum(x, dims=3)
+    x = relu.(x)
+    return x
+end
+
+emb = GraphEmb(10, 2)
+x = randn(20, 2, 5)
+
+display(size(emb(x)))
+
+# display(Threads.nthreads())
+#
+# env = CylinderEnv(M=10, continuous=true, grid_size=10.0)
+#
+# min_M = 3
+# max_M = 10
+#
+# envs = Vector{CylinderEnv}(undef, 100)
+#
+# @time begin
+#     Threads.@threads for i = 1 : length(envs)
+#         envs[i] = CylinderEnv(M=rand(min_M:max_M), continuous=false, grid_size=10.0)
+#     end
+#
+#     Threads.@threads for i = 1 : length(envs)
+#         reset!(envs[i])
+#     end
+# end
