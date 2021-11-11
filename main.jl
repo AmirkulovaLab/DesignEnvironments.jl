b308b697ce7b92633f33440f142ab0f0e295a664
Tristan Shah <123.tristan.shah@gmail.com>
2021-11-10 19:46:52 -0500

    cleanup

--- main.jl
+++ main.jl
@@ -1,57 +1,9 @@
-# ENV["GKSwstype"] = "nul"
+ENV["GKSwstype"] = "nul"
 
 using DesignEnvironments
 using ReinforcementLearning
 
-env = Physics(CylinderEnv(M=10, grid_size=10, k0amax=2.0, nfreq=20, continuous=false))
+env = CylinderEnv(M=10, grid_size=10.0, continuous=false, max_velocity=0.1)
+policy = RandomPolicy(action_space(env))
 
-# policy = RandomPolicy(action_space(env))
-# render(env, policy, path="thing.mp4", max_tscs=30.0)
-
-collisions = DesEnv.get_collisions(env)
-DesEnv.resolve_cylinder_collisions(env)
-
-function resolve_wall_collisions(env::Physics{CylinderEnv})
-    params = get_params(env)
-    coords = get_coords(env)
-
-    ## radius size is assumed to be 1
-    center_bound = params.grid_size - 1
-
-    left_collision = coords[:, 1] .< - center_bound
-    right_collision = coords[:, 1] .> center_bound
-
-    coords[left_collision, 1] .= - center_bound
-    coords[right_collision, 1] .= center_bound
-    env.vel[]
-
-    display(left_collision)
-    println()
-    display(right_collision)
-end
-
-resolve_wall_collisions(env)
-display(img(env))
-
-#
-# def resolve_wall_collisions(self):
-#         '''
-#         Resolves cylinder collisions with walls.
-#         '''
-#         center_bound = self.grid_size - self.cyl_radii
-#
-#         left_collision = (self.config[:, 0] - self.cyl_radii < -self.grid_size)
-#         right_collision = (self.config[:, 0] + self.cyl_radii > self.grid_size)
-#
-#         self.config[left_collision, 0] = -center_bound
-#         self.config[right_collision, 0] = center_bound
-#         self.vel[left_collision, 0] *= -self.wall_bounce
-#         self.vel[right_collision, 0] *= -self.wall_bounce
-#
-#         top_collision = (self.config[:, 1] + self.cyl_radii > self.grid_size)
-#         bottom_collision = (self.config[:, 1] - self.cyl_radii < -self.grid_size)
-#
-#         self.config[top_collision, 1] = center_bound
-#         self.config[bottom_collision, 1] = -center_bound
-#         self.vel[top_collision, 1] *= -self.wall_bounce
-#         self.vel[bottom_collision, 1] *= -self.wall_bounce
+DE.render(env, policy, path="new.mp4", max_tscs=10.0)
--- test.jl
+++
@@ -1,9 +0,0 @@
-ENV["GKSwstype"] = "nul"
-
-using DesignEnvironments
-using ReinforcementLearning
-
-env = CylinderEnv(M=10, grid_size=10.0, continuous=false, max_velocity=0.1)
-policy = RandomPolicy(action_space(env))
-
-DE.render(env, policy, path="new.mp4", max_tscs=10.0)
