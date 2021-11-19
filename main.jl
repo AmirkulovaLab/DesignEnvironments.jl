bc7bb32fbed0997a21c1308c3ea203c173d43e80
Tristan Shah <123.tristan.shah@gmail.com>
2021-11-19 11:46:58 -0500

    env works

--- main.jl
+++ main.jl
@@ -7,13 +7,34 @@ env = CylinderEnv(
     M=10,
     grid_size=8.0,
     k0amax=1.0,
-    continuous=false,
+    continuous=true,
     step_size=0.1,
-    nfreq=30,
-    velocity_decay=0.1,
+    nfreq=20,
+    velocity_decay=1.0,
     physics=true)
 
-policy = RandomPolicy(action_space(env))
+# policy = RandomPolicy(action_space(env))
+
+mutable struct DummyPolicy <: AbstractPolicy
+    action
+    count::Int
+
+    function DummyPolicy(env::CylinderEnv)
+        action = env |> action_space |> rand
+        return new(action, 0)
+    end
+end
+
+function (policy::DummyPolicy)(env::CylinderEnv)
+    if policy.count >= 1
+        policy.action = zeros(size(action_space(env)))
+    end
+
+    policy.count += 1
+    return policy.action
+end
+
+policy = DummyPolicy(env)
 
 DE.render(env, policy, path="new.mp4", max_tscs=30.0, fps=15)
 
